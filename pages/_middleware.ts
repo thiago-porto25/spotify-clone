a83876f37be5511350b0bef58984f24ea281546b
Thiago Porto <thiago.porto@d3.do>
2022-04-30 10:50:12 -0300

    create redirect middleware

---
+++ pages/_middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.JWT_SECRET })
+  const url = req.nextUrl.clone()
+
+  if (token && req.nextUrl.pathname === '/login') {
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+  if (req.nextUrl.pathname.includes('/api/auth') || token) {
+    return NextResponse.next()
+  }
+
+  if (!token && req.nextUrl.pathname !== '/login') {
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  }
+
+  return NextResponse.next()
+}
